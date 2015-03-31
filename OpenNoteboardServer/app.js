331eec1de9a32515990ebeb704ae7a5faf7bf3a9
Avi Revivo <avi@revivo.co.il>
2015-03-31 20:17:35 +0300

    fixed basic issues

--- OpenNoteboardServer/app.js
+++ OpenNoteboardServer/app.js
@@ -1,30 +1,27 @@
 // loading all relevant component
 var express = require('express');
 var notes = require('./notes');
-var mongo = require('mongodb');
-var socket = require('socket.io');
-var http = require('http');
-var path = require('path');
+var bodyParser = require('body-parser');
+
+var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
+var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
 
 // setting up express and its middle-ware
 var app = express();
-var publicContentPath = path.join(__dirname, 'public');
 var oneDay = 86400000;
-app.configure(function () {
-    app.use(express.logger('default'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-    app.use(express.compress());
-    // serving static content
-    //app.use('/', express.static(publicContentPath, { maxAge: oneDay }));
-    app.use('/', express.static('..\\OpenNoteboard', { maxAge: oneDay }));
-});
 
-// opening for web sockets
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+//app.use(express.compress());
+
+var directory = __dirname + '\\..\\OpenNoteboard';
+console.log('serving static content from:' + directory);
+app.use(express.static(directory,{ maxAge: oneDay }));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 // initializing the notes rest api requests
-app.all('/notes', notes.options);
+//app.all('/notes', notes.options);
 app.post('/notes', function(req,res){
     notes.addNewNote(req,res,io);
 });
@@ -34,11 +31,11 @@ app.delete('/notes/:id', function(req,res)
     notes.deleteNote(req,res,io);
 });
 
-// starting to listen to http requests
-var portToListen = process.env.PORT || 8080;
-server.listen(portToListen);
+// start serving static content and start socket.io
+var server = app.listen(port);
+var io = require('socket.io').listen(server);
 
-console.log('serving on port: ' + portToListen + ', public folder: ' + publicContentPath);
+console.log('serving on port: ' + port);
 
 
 
--- OpenNoteboardServer/notes.js
+++ OpenNoteboardServer/notes.js
@@ -1,9 +1,7 @@
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var mongojs = require('mongojs');
 var url = require('url');
-
-var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
-var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+var bson = require("bson");
+var BSON = bson.BSONPure.BSON;
 
 // default to a 'localhost' configuration:
 var connection_string = '127.0.0.1:27017/open';
@@ -16,124 +14,90 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
     process.env.OPENSHIFT_APP_NAME;
 }
 
-var BSON = MongoClient.BSONPure;
+var db = mongojs(connection_string, ['notes']);
+var notes = db.collection('notes');
 
-//var server = new Server('mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/', 27017, {auto_reconnect: true});
-//db = new Db('notesdb', server);
-//db.open(function(err, db) {
-//    if(!err) {
-//        console.log("Connected to 'notesdb' database");
+//exports.options = function(req,res,next)
+//{
+//    if(req.route.method == "options")
+//    {
+//        console.log('!OPTIONS');
+//        var headers = {};
+//        // the below allows cross site scripting
+//        // IE8 does not allow domains to be specified, just the *
+//        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+//        // headers["Access-Control-Allow-Origin"] = "*";
+//        // headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+//        // headers["Access-Control-Allow-Credentials"] = false;
+//        // headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+//        // headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+//        res.writeHead(200, headers);
+//        res.end();
 //    }
-//});
+//
+//    next(); // allow to route for POST, GET and others
+//}
 
-console.log(connection_string);
+exports.addNewNote = function(req, res, io)
+{
+    var note = req.body;
 
-// Use connect method to connect to the Server
-MongoClient.connect(connection_string, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+    console.log('Adding note: ' + JSON.stringify(note));
 
-    exports.options = function(req,res,next)
+    db.notes.save(note);
+
+    // sending an event that the board was updated to all registered clients
+    var eventName = 'board_'+note['board']; // todo: make prefix constant
+    console.log('emiting event: ' + eventName);
+    io.sockets.emit(eventName, {});
+
+    res.end();
+};
+
+exports.findAllInBoard = function(req, res)
+{
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    if(query['board'])
     {
-        if(req.route.method == "options")
+        db.notes.find({
+            board: query['board']
+        }).toArray(function(err, docs) {
+                res.send(docs);
+            });
+    }
+    else
+    {
+        // closing response if no specific board was requested.
+        // Not enabling someone querying for all existing boards.
+        res.end();
+    }
+};
+
+exports.deleteNote = function(req, res, io) {
+
+    var o_id = db.ObjectId(req.params.id);
+
+    console.log('Attempting to delete note: ' + o_id);
+
+    // delete a specific thing.
+    db.notes.findAndModify({query: {_id: o_id}, remove:true}, function(err, object, lastErrorObject)
         {
-            console.log('!OPTIONS');
-            var headers = {};
-            // the below allows cross site scripting
-            // IE8 does not allow domains to be specified, just the *
-            // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-            // headers["Access-Control-Allow-Origin"] = "*";
-            // headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-            // headers["Access-Control-Allow-Credentials"] = false;
-            // headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-            // headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-            res.writeHead(200, headers);
+            if (err)
+            {
+                console.log('An error has occurred - err=' + err + ', lastErrorObject=' + lastErrorObject);
+            }
+            else if(object)
+            {
+                console.log('' + object['_id'] + ' document deleted');
+
+                // sending an event that the board was updated to all registered clients
+                var eventName = 'board_' + object['board']; // todo: make prefix constant
+                io.sockets.emit(eventName, {});
+            }
+
             res.end();
         }
-
-        next(); // allow to route for POST, GET and others
-    }
-
-    exports.addNewNote = function(req, res, io)
-    {
-        var note = req.body;
-
-        console.log('Adding note: ' + JSON.stringify(note));
-
-        db.collection('notes', function(err, collection) {
-            collection.insert(note, {safe:true}, function(err, result) {
-                if (err) {
-                    res.send({'error':'An error has occurred'});
-                } else {
-                    console.log('Success: ' + JSON.stringify(result[0]));
-                    res.send(result[0]);
-                }
-            });
-        });
-
-        // sending an event that the board was updated to all registered clients
-        var eventName = 'board_'+note['board']; // todo: make prefix constant
-        io.sockets.emit(eventName, {});
-    };
-
-    exports.findAllInBoard = function(req, res)
-    {
-        var url_parts = url.parse(req.url, true);
-        var query = url_parts.query;
-
-        if(query['board'])
-        {
-            db.collection('notes', function(err, collection) {
-                collection.find({ board: query['board'] }).toArray(function(err, items) {
-                    res.send(items);
-                });
-            });
-        }
-        else
-        {
-            // closing response if no specific board was requested.
-            // Not enabling someone querying for all existing boards.
-            res.end();
-        }
-    };
-
-    exports.deleteNote = function(req, res, io) {
-
-        var id = req.params.id;
-
-        console.log('Attempting to delete note: ' + id);
-        db.collection('notes', function(err, collection) {
-
-            var o_id = new BSON.ObjectID(id);
-            collection.findAndModify({'_id': o_id}, [], {}, {remove:true}, function(err, object)
-                {
-                    if (err)
-                    {
-                        res.send({'error':'An error has occurred - ' + err});
-                    }
-                    else if(object)
-                    {
-                        console.log('' + object['_id'] + ' document deleted');
-                        res.send(req.body);
-
-                        // sending an event that the board was updated to all registered clients
-                        var eventName = 'board_' + object['board']; // todo: make prefix constant
-                        io.sockets.emit(eventName, {});
-                    }
-                    else if(!object)
-                    {
-                        res.end();
-                    }
-                }
-            );
-        });
-    }
-
-    db.close();
-});
-
-
-
-
-
-
+    );
+}
